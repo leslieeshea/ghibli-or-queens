a58ac614655de8cdef03efac8786c53e9886d984
Leslie Shea <leslieyshea@gmail.com>
2019-03-04 17:13:53 -0800

    moved makeFilmDetail function to new file in src

--- test/make-film-detail.test.js
+++ test/make-film-detail.test.js
@@ -1,29 +1,8 @@
+import { makeFilmDetail } from '../src/film-detail/film-detail-component.js';
+
 const test = QUnit.test;
 
 QUnit.module('generate film detail');
-
-function makeFilmDetail(film) {
-    const html = /*html*/
-    `<dl>
-        <dt>Title:</dt>
-            <dd>${film.title}</dd>
-        <dt>Description:</dt>
-            <dd>${film.description}</dd>
-        <dt>Director:</dt>
-            <dd>${film.director}</dd>
-        <dt>Producer:</dt>
-            <dd>${film.producer}</dd>
-        <dt>Release Date:</dt>
-            <dd>${film.release_date}</dd>
-        <dt>RT Score:</dt>
-            <dd>${film.rt_score}</dd>
-    </dl>`;
-
-    const template = document.createElement('template');
-    template.innerHTML = html;
-
-    return template.content;
-}
 
 const film = {
     title: 'Castle in the Sky',
