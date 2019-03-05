9a7fe3976a78af9dfdc65356f5c2f035c06a584e
Leslie Shea <leslieyshea@gmail.com>
2019-03-04 17:36:49 -0800

    added comments for clarity

--- src/list/index.js
+++ src/list/index.js
@@ -3,11 +3,11 @@ import loadFilms from './film-list-component.js';
 const URL = 'https://ghibliapi.herokuapp.com/films';
 
 fetch(URL)
-    .then(response => response.json())
+    .then(response => response.json()) // get the body from json
     .then(films => {
-        loadFilms(films);
+        loadFilms(films); // once you have the json, run function to load films onto page
     })
     .catch(err => {
         /* eslint-disable-next-line */
-        console.error('Fetch error:', err);
+        console.error('Fetch error:', err); // if something goes wrong, show error message
     });
